7293b7943acd246957ff2e2421b3ce2ee3701837
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-08-21 15:26:01 -0300

    Adicionar arquivos de projeto.

---
+++ AlunosCrud/Controllers/AlunoController.cs
@@ -0,0 +1,36 @@
+﻿using AlunosCrud.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosCrud.Controllers
+{
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Aluno aluno)
+        {
+           return Ok(aluno);
+        }
+
+        [HttpPut]
+        public ActionResult Edit(Aluno aluno)
+        {    
+             return Ok(aluno);  
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
+        }
+        
+    }
+}
---
+++ AlunosCrud/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace AlunosCrud.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Ra { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
